d504853c10422154013f1915358f26969a1d2eb2
Daniel Prosser <dpross1100@msn.com>
2018-01-31 00:44:11 -0500

    Improve python example
    Change airfoil, operating points, and add plots

--- examples/python_example.py
+++ examples/python_example.py
@@ -3,47 +3,80 @@
 import xfoil_interface_wrap as xiw
 from xfoil_interface import xfoil_options_type, xfoil_geom_options_type
 from matplotlib import pyplot as plt
-import sys
 
-def read_airfoil(filename):
+def plot_airfoil(x, z, name):
 
-  try:
-    f = open(filename)
-  except IOError:
-    print("Error: unable to open {:s}.".format(filename))
-    sys.exit(1)
+  plt.clf()
+  plt.close()
 
-  x = []
-  z = []
-  npoint = 0
-  for line in f:
-    x.append(float(line.split()[0]))
-    z.append(float(line.split()[1]))
-    npoint += 1
+  fig, ax = plt.subplots()
+  ax.set_xlabel('x')
+  ax.set_ylabel('z')
+  ax.set_title(name)
+  ax.plot(x, z)
+  ax.grid()
+  ax.set_aspect('equal', 'datalim')
 
-  return x, z, npoint
+  plt.show() 
+
+def plot_polars(alpha, cl, cd, cm, xtrt, xtrb):
+
+  plt.clf()
+  plt.close()
+
+  fig, axarr = plt.subplots(2,2)
+  fig.suptitle('Notice effect of flap deflection!')
+
+  axarr[0,0].set_xlabel('Angle of attack')
+  axarr[0,0].set_ylabel('Lift coefficient')
+  axarr[0,0].plot(alpha, cl)
+  axarr[0,0].grid()
+
+  axarr[0,1].set_xlabel('Drag coefficient')
+  axarr[0,1].set_ylabel('Lift coefficient')
+  axarr[0,1].plot(cd, cl)
+  axarr[0,1].grid()
+
+  axarr[1,0].set_xlabel('Angle of attack')
+  axarr[1,0].set_ylabel('Pitching moment coefficient')
+  axarr[1,0].plot(alpha, cm)
+  axarr[1,0].grid()
+
+  axarr[1,1].set_xlabel('Angle of attack')
+  axarr[1,1].set_ylabel('Transition locations')
+  axarr[1,1].plot(alpha, xtrt)
+  axarr[1,1].plot(alpha, xtrb)
+  axarr[1,1].grid()
+  axarr[1,1].legend(['Top', 'Bottom'], loc='center left')
+
+  plt.show()
 
 if __name__ == "__main__":
 
-  x, z, npoint = read_airfoil("clarky.dat")
+  digits = '2312'
+  x, z, npoint = xiw.naca_4_digit('2312', 100)
+  plot_airfoil(x, z, 'NACA ' + digits)
 
-  noppoint = 8
-  oppoints = [-6., -3., 0., 3., 6., 9., 12., 15.]
-  opmodes = noppoint*[0]
-  re = noppoint*[3.E+05]
-  mach = noppoint*[0.3]
-  flapang = noppoint*[0.]
-  use_flap = False
+  noppoint = 10
+  oppoints = [-0.5, -0.25, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
+  opmodes = noppoint*[1]
+  re = noppoint*[3.E+06]
+  mach = noppoint*[0.4]
+  flapang = [0., 0., 0., 0., 0., 0., 3., 6., 9., 12., 15.]
+  use_flap = True
+  x_flap = 0.7
+  y_flap = 0.0
+  y_flap_spec = 0
 
   opts = xfoil_options_type()
   opts.ncrit = 9.
   opts.xtript = 1.
   opts.xtripb = 1.
   opts.viscous_mode = True
-  opts.silent_mode = False
+  opts.silent_mode = True
   opts.maxit = 100
   opts.vaccel = 0.01
-  opts.fix_unconverged = False
+  opts.fix_unconverged = True
   opts.reinitialize = False
 
   geom_opts = xfoil_geom_options_type()
@@ -56,13 +89,16 @@ if __name__ == "__main__":
   geom_opts.xpref1 = 1.
   geom_opts.xpref2 = 1.
 
+  print("Calculating aerodynamics with Xfoil ...")
   xiw.xfoil_init()
   lift, drag, moment, viscrms, alpha, xtrt, xtrb = \
     xiw.run_xfoil(npoint, x, z, geom_opts, noppoint, oppoints, opmodes, re,
-                  mach, use_flap, 0., 0., 0, flapang, opts)
+                  mach, use_flap, x_flap, y_flap, y_flap_spec, flapang, opts)
 
   for i in range(noppoint):
     print("Point {:d}: AoA = {:<.4f}, Cl = {:<.4f}, Cd = {:<.4f}, \
 Cm = {:6.4f}".format(i, alpha[i], lift[i], drag[i], moment[i]))
 
   xiw.xfoil_cleanup()
+
+  plot_polars(alpha, lift, drag, moment, xtrt, xtrb)
