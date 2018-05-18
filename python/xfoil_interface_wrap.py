montagdude/libxfoil

commit 4cd1a47be4b836213d0bc907638281f2f493a87a

Get rid of ncrit_per_point in run_xfoil; rename y_flap to z_flap
If users need to change ncrit at each operating point, they can just
implement a loop manually instead of using the run_xfoil subroutine.

python/xfoil_interface_wrap.py
@@ -64,24 +64,24 @@ def xfoil_smooth_paneling(xdg):
 
   return stat
 
-def xfoil_apply_flap_deflection(xdg, xflap, yflap, y_flap_spec, degrees):
+def xfoil_apply_flap_deflection(xdg, xflap, zflap, z_flap_spec, degrees):
 
   xflap_p = xi.copy_doublep(xflap)
-  yflap_p = xi.copy_doublep(yflap)
-  y_flap_spec_p = xi.copy_intp(y_flap_spec)
+  zflap_p = xi.copy_doublep(zflap)
+  z_flap_spec_p = xi.copy_intp(z_flap_spec)
   degrees_p = xi.copy_doublep(degrees)
   npointout_p = xi.new_intp()
   stat_p = xi.new_intp()
 
-  xi.xfoil_apply_flap_deflection(xdg, xflap_p, yflap_p, y_flap_spec_p,
+  xi.xfoil_applz_flap_deflection(xdg, xflap_p, zflap_p, z_flap_spec_p,
                                  degrees_p, npointout_p, stat_p)
 
   npointout = xi.intp_value(npointout_p)
   stat = xi.intp_value(stat_p)
 
   xi.delete_doublep(xflap_p)
-  xi.delete_doublep(yflap_p)
-  xi.delete_intp(y_flap_spec_p)
+  xi.delete_doublep(zflap_p)
+  xi.delete_intp(z_flap_spec_p)
   xi.delete_intp(npointout_p)
   xi.delete_intp(stat_p)
 
@@ -384,8 +384,8 @@ def xfoil_get_ampl(xdg, npoint):
 
 def run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,
               op_modes, reynolds_numbers, mach_numbers, use_flap, x_flap,
-              y_flap, y_flap_spec, flap_degrees, xfoil_opts, reinitialize,
-              fix_unconverged, ncrit_per_point=None):
+              z_flap, z_flap_spec, flap_degrees, xfoil_opts, reinitialize,
+              fix_unconverged):
 
   npointin_p = xi.copy_intp(npointin)
   xin_a = xi.new_doublea(npointin)
@@ -398,12 +398,12 @@ def run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,
   use_flap_p = xi.copy_boolp(use_flap)
   if use_flap:
     x_flap_p = xi.copy_doublep(x_flap)
-    y_flap_p = xi.copy_doublep(y_flap)
-    y_flap_spec_p = xi.copy_intp(y_flap_spec)
+    z_flap_p = xi.copy_doublep(z_flap)
+    z_flap_spec_p = xi.copy_intp(z_flap_spec)
   else:
     x_flap_p = xi.new_doublep()
-    y_flap_p = xi.new_doublep()
-    y_flap_spec_p = xi.new_intp()
+    z_flap_p = xi.new_doublep()
+    z_flap_spec_p = xi.new_intp()
   flap_degrees_a = xi.new_doublea(noppoint)
   reinitialize_p = xi.copy_boolp(reinitialize)
   fix_unconverged_p = xi.copy_boolp(fix_unconverged)
@@ -415,7 +415,6 @@ def run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,
   xtrt_a = xi.new_doublea(noppoint)
   xtrb_a = xi.new_doublea(noppoint)
   stat_p = xi.new_intp()
-  ncrit_per_point_a = xi.new_doublea(noppoint) 
 
   for i in range(npointin):
     xi.doublea_setitem(xin_a, i, xin[i])
@@ -427,23 +426,13 @@ def run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,
     xi.doublea_setitem(mach_numbers_a, i, mach_numbers[i])
     if use_flap:
       xi.doublea_setitem(flap_degrees_a, i, flap_degrees[i])
-    if ncrit_per_point is not None:
-      xi.double_setitem(ncrit_per_point_a, i, ncrit_per_point[i]) 
 
-  if ncrit_per_point is not None:
-    xi.run_xfoil(npointin_p, xin_a, zin_a, geom_opts, noppoint_p,
-                 operating_points_a, op_modes_a, reynolds_numbers_a,
-                 mach_numbers_a, use_flap_p, x_flap_p, y_flap_p, y_flap_spec_p,
-                 flap_degrees_a, xfoil_opts, reinitialize_p, fix_unconverged_p,
-                 lift_a, drag_a, moment_a, viscrms_a, alpha_a, xtrt_a, xtrb_a,
-                 stat_p, ncrit_per_point_a)
-  else:
-    xi.run_xfoil(npointin_p, xin_a, zin_a, geom_opts, noppoint_p,
-                 operating_points_a, op_modes_a, reynolds_numbers_a,
-                 mach_numbers_a, use_flap_p, x_flap_p, y_flap_p, y_flap_spec_p,
-                 flap_degrees_a, xfoil_opts, reinitialize_p, fix_unconverged_p,
-                 lift_a, drag_a, moment_a, viscrms_a, alpha_a, xtrt_a, xtrb_a,
-                 stat_p)
+  xi.run_xfoil(npointin_p, xin_a, zin_a, geom_opts, noppoint_p,
+               operating_points_a, op_modes_a, reynolds_numbers_a,
+               mach_numbers_a, use_flap_p, x_flap_p, z_flap_p, z_flap_spec_p,
+               flap_degrees_a, xfoil_opts, reinitialize_p, fix_unconverged_p,
+               lift_a, drag_a, moment_a, viscrms_a, alpha_a, xtrt_a, xtrb_a,
+               stat_p)
 
   stat = xi.intp_value(stat_p)
   lift = noppoint*[0]
@@ -472,8 +461,8 @@ def run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,
   xi.delete_doublea(mach_numbers_a)
   xi.delete_boolp(use_flap_p)
   xi.delete_doublep(x_flap_p)
-  xi.delete_doublep(y_flap_p)
-  xi.delete_intp(y_flap_spec_p)
+  xi.delete_doublep(z_flap_p)
+  xi.delete_intp(z_flap_spec_p)
   xi.delete_doublea(flap_degrees_a)
   xi.delete_doublea(lift_a)
   xi.delete_doublea(drag_a)
@@ -483,7 +472,6 @@ def run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,
   xi.delete_doublea(xtrt_a)
   xi.delete_doublea(xtrb_a)
   xi.delete_intp(stat_p)
-  xi.delete_doublea(ncrit_per_point_a)
 
   return lift, drag, moment, viscrms, alpha, xtrt, xtrb, stat
 

examples/c_example.c
@@ -19,7 +19,6 @@ int main()
   double x_flap, y_flap;
   int y_flap_spec;
   double flap_degrees[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
-  double ncrit_per_point[5] = {9.0, 9.0, 9.0, 9.0, 9.0};
   bool reinitialize = false;
   bool fix_unconverged = false;
   bool converged;
@@ -80,7 +79,7 @@ int main()
   run_xfoil(&npoint, x, z, &geom_opts, &noppoint, oppoints, opmodes, re, mach,
             &use_flap, &x_flap, &y_flap, &y_flap_spec, flap_degrees, &opts,
             &reinitialize, &fix_unconverged, lift, drag, moment, viscrms, alpha,
-            xtrt, xtrb, &stat, ncrit_per_point);
+            xtrt, xtrb, &stat);
   if (stat != 0) { return stat; }
 
   for ( i = 0; i < noppoint; i++ )

python/xfoil_interface.i
@@ -177,8 +177,8 @@ extern void xfoil_set_airfoil(xfoil_data_group *xdg, double *xin, double *zin,
                               int *npointin);
 extern void xfoil_smooth_paneling(xfoil_data_group *xdg, int *stat);
 extern void xfoil_apply_flap_deflection(xfoil_data_group *xdg,
-                                        double *xflap, double *yflap,
-                                        int *y_flap_spec, double *degrees,
+                                        double *xflap, double *zflap,
+                                        int *z_flap_spec, double *degrees,
                                         int *npointout, int *stat);
 extern void xfoil_modify_tegap(xfoil_data_group *xdg, double *gap,
                                double *blendloc, int *npointout, int *stat);
@@ -218,13 +218,12 @@ extern void run_xfoil(int *npointin, double *xin, double *zin,
                       xfoil_geom_options_type *geom_opts, int *noppoint,
                       double *operating_points, int *op_modes,
                       double *reynolds_numbers, double *mach_numbers,
-                      bool *use_flap, double *x_flap, double *y_flap,
-                      int *y_flap_spec, double *flap_degrees,
+                      bool *use_flap, double *x_flap, double *z_flap,
+                      int *z_flap_spec, double *flap_degrees,
                       xfoil_options_type *xfoil_opts, bool *reinitialize,
                       bool *fix_unconverged, double *lift, double *drag,
                       double *moment, double *viscrms, double *alpha,
-                      double *xtrt, double *xtrb, int *stat,
-                      double *ncrit_per_point);
+                      double *xtrt, double *xtrb, int *stat);
 extern void naca_4_digit(char *des, int *npointside, double *xout,
                          double *zout, int *nout);
 extern void naca_5_digit(char *des, int *npointside, double *xout,

src/xfoil_interface.f90
@@ -189,18 +189,18 @@ end subroutine xfoil_smooth_paneling
 !
 ! Subroutine to apply a flap deflection to the buffer airfoil and set it as the
 ! current airfoil. It is recommended to call this after xfoil_smooth_paneling.
-! y_flap_spec = 0: specified as y/c
+! z_flap_spec = 0: specified as y/c
 !             = 1: specified as y/local thickness
 ! stat: 0 for success, 1 for failure (xfoil_set_airfoil not called yet)
 !
 !=============================================================================80
-subroutine xfoil_apply_flap_deflection(xdg, xflap, yflap, y_flap_spec, degrees,&
+subroutine xfoil_apply_flap_deflection(xdg, xflap, zflap, z_flap_spec, degrees,&
                                        npointout, stat)                        &
            bind(c, name="xfoil_apply_flap_deflection")
 
   type(xfoil_data_group), intent(inout) :: xdg
-  real(c_double), intent(in) :: xflap, yflap, degrees
-  integer(c_int), intent(in) :: y_flap_spec
+  real(c_double), intent(in) :: xflap, zflap, degrees
+  integer(c_int), intent(in) :: z_flap_spec
   integer(c_int), intent(out) :: npointout, stat
 
 ! Check that buffer airfoil is set
@@ -213,7 +213,7 @@ subroutine xfoil_apply_flap_deflection(xdg, xflap, yflap, y_flap_spec, degrees,&
 
 ! Apply flap deflection
 
-  call FLAP(xdg%xfd, xflap, yflap, y_flap_spec, degrees)
+  call FLAP(xdg%xfd, xflap, zflap, z_flap_spec, degrees)
 
 ! Get new buffer airfoil points (may have changed)
 
@@ -753,30 +753,45 @@ end subroutine xfoil_get_ampl
 
 !=============================================================================80
 !
-! Subroutine to get Cl, Cd, Cm for an airfoil from Xfoil at given operating
-! conditions.  Reynolds numbers and mach numbers should be specified for each
-! operating point.  Additionally, op_mode determines whether each point is run
-! at a constant alpha or cl - use 0 for specified alpha and 1 for specified cl.
+! Subroutine to analyze an airfoil in a loop over a number of operating points.
+!
+! Inputs:
+!   npointin: number of points in airfoil coordinates
+!   xin, zin: airfoil coordinates
+!   geom_opts: Xfoil paneling settings
+!   noppoints: number of operating points to analyze
+!   operating_points: specified AoA or Cl for each point
+!   op_modes: indicates whether each operating_point specifies an AoA or Cl.
+!     0 => AoA, 1 => Cl
+!   reynolds_numbers, mach_numbers: Re and Mach for each operating point
+!   use_flap: T or F; whether flap deflections will be applied
+!   x_flap, z_flap: x and z flap hinge coordinates
+!   z_flap_spec: 0 => z_flap = z/c, 1 => z_flap = z/local_thickness
+!   flap_degrees: flap deflection at each operating point (+ve down)
+!   xfoil_opts: Xfoil run settings
+!   reinitialize: T or F; whether to always reinitialize the BL at each point
+!   fix_unconverged: T or F; if true, for any points that fail to converge,
+!     will try to initialize the BL at a point closer to Cl = 0, and then
+!     run again at the original operating point
 !
 ! Outputs:
-!   alpha, Cl, Cd, Cm each operating point
+!   alpha, Cl, Cd, Cm, and x/c transition locations at each operating point
 !   viscrms: rms for viscous calculations (check for convergence)
 !
 !=============================================================================80
 subroutine run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,&
                      op_modes, reynolds_numbers, mach_numbers, use_flap,       &
-                     x_flap, y_flap, y_flap_spec, flap_degrees, xfoil_opts,    &
+                     x_flap, z_flap, z_flap_spec, flap_degrees, xfoil_opts,    &
                      reinitialize, fix_unconverged, lift, drag, moment,        &
-                     viscrms, alpha, xtrt, xtrb, stat, ncrit_per_point)        &
-                     bind(c, name="run_xfoil")
+                     viscrms, alpha, xtrt, xtrb, stat) bind(c, name="run_xfoil")
 
   integer(c_int), intent(in) :: npointin, noppoint
   real(c_double), dimension(npointin), intent(in) :: xin, zin
   type(xfoil_geom_options_type), intent(in) :: geom_opts
   real(c_double), dimension(noppoint), intent(in) :: operating_points,         &
                                     reynolds_numbers, mach_numbers, flap_degrees
-  real(c_double), intent(in) :: x_flap, y_flap
-  integer(c_int), intent(in) :: y_flap_spec
+  real(c_double), intent(in) :: x_flap, z_flap
+  integer(c_int), intent(in) :: z_flap_spec
   type(xfoil_options_type), intent(in) :: xfoil_opts
   logical(c_bool), intent(in) :: use_flap, reinitialize, fix_unconverged
   integer(c_int), dimension(noppoint), intent(in) :: op_modes
@@ -784,7 +799,6 @@ subroutine run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,&
                                                       viscrms
   real(c_double), dimension(noppoint), intent(out) :: alpha, xtrt, xtrb
   integer(c_int), intent(out) :: stat
-  real(c_double), dimension(noppoint), intent(in), optional :: ncrit_per_point
 
   type(xfoil_data_group) :: xdg
   integer(c_int) :: i, dummy 
@@ -830,7 +844,7 @@ subroutine run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,&
     if (use_flap) then
       call xfoil_set_airfoil(xdg, xin, zin, npointin)
       call xfoil_smooth_paneling(xdg, stat)
-      call xfoil_apply_flap_deflection(xdg, x_flap, y_flap, y_flap_spec,       &
+      call xfoil_apply_flap_deflection(xdg, x_flap, z_flap, z_flap_spec,       &
                                        flap_degrees(i), dummy, stat)
     end if
 
@@ -838,10 +852,6 @@ subroutine run_xfoil(npointin, xin, zin, geom_opts, noppoint, operating_points,&
     call xfoil_set_mach_number(xdg, mach_numbers(i))
 
     if (reinitialize) call xfoil_reinitialize_bl(xdg)
-
-!   Set ncrit per point
-
-    if (present(ncrit_per_point)) xdg%xfd%ACRIT = ncrit_per_point(i)
 
     if (op_modes(i) == 0) then
 

src/xfoil_interface.h
@@ -170,8 +170,8 @@ extern void xfoil_set_airfoil(xfoil_data_group *xdg, const double xin[],
 extern void xfoil_smooth_paneling(xfoil_data_group *xdg, int *stat);
 extern void xfoil_apply_flap_deflection(xfoil_data_group *xdg,
                                         const double *xflap,
-                                        const double *yflap,
-                                        const int *y_flap_spec,
+                                        const double *zflap,
+                                        const int *z_flap_spec,
                                         const double *degrees, int *npointout,
                                         int *stat);
 extern void xfoil_modify_tegap(xfoil_data_group *xdg, const double *gap,
@@ -220,13 +220,13 @@ extern void run_xfoil(const int *npointin, const double xin[],
                       const int *noppoint, const double operating_points[],
                       const int op_modes[], const double reynolds_numbers[],
                       const double mach_numbers[], const bool *use_flap,
-                      const double *x_flap, const double *y_flap,
-                      const int *y_flap_spec, const double flap_degrees[],
+                      const double *x_flap, const double *z_flap,
+                      const int *z_flap_spec, const double flap_degrees[],
                       const xfoil_options_type *xfoil_opts, 
                       const bool *reinitialize, const bool *fix_unconverged,
                       double lift[], double drag[], double moment[],
                       double viscrms[], double alpha[], double xtrt[],
-                      double xtrb[], int *stat, const double ncrit_per_point[]);
+                      double xtrb[], int *stat);
 extern void naca_4_digit(const char des[4], const int *npointside,
                          double xout[], double zout[], int *nout);
 extern void naca_5_digit(const char des[5], const int *npointside,

commit a9de2c78160bbcfda496ea03b1be4b14a4810452

Fix typo in xfoil_interface_wrap.py introduced in 4cd1a47be

python/xfoil_interface_wrap.py
@@ -73,7 +73,7 @@ def xfoil_apply_flap_deflection(xdg, xflap, zflap, z_flap_spec, degrees):
   npointout_p = xi.new_intp()
   stat_p = xi.new_intp()
 
-  xi.xfoil_applz_flap_deflection(xdg, xflap_p, zflap_p, z_flap_spec_p,
+  xi.xfoil_apply_flap_deflection(xdg, xflap_p, zflap_p, z_flap_spec_p,
                                  degrees_p, npointout_p, stat_p)
 
   npointout = xi.intp_value(npointout_p)